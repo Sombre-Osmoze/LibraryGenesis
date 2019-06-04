0ce59e2a8330b204ffb9ae06b63566e05a9e5b70
Marcus Florentin <fmarcus28@gmx.fr>
2019-06-05 01:40:50 +0200

    Initial Commit

---
+++ Library GenesisTests/Library_GenesisTests.swift
@@ -0,0 +1,34 @@
+//
+//  Library_GenesisTests.swift
+//  Library GenesisTests
+//
+//  Created by Marcus Florentin on 05/06/2019.
+//  Copyright © 2019 Marcus Florentin. All rights reserved.
+//
+
+import XCTest
+@testable import Library_Genesis
+
+class Library_GenesisTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
